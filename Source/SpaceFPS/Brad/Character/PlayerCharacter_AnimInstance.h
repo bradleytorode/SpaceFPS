2910207d8e8c64172f3163b12394c09bcc840297
Brad <1801835@student.nua.ac.uk>
2021-04-30 11:03:45 +0100

    Animations Slighlty Messed up. But gun works as intended. Just need to add the Ammo and change animations in order to make it feel better for the player

---
+++ Source/SpaceFPS/Brad/Character/PlayerCharacter_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerCharacter_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEFPS_API UPlayerCharacter_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
